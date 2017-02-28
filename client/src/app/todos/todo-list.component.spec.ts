661fc2a44e087624e30e8dd4d7fc66c32af72c4c
antin006 <antin006@morris.umn.edu>
2017-02-28 02:35:04 -0600

    Finish lab with tests and glyphicons and labels
    The bootstrap features have been implemented and Angular tests have been implemented as well.
    
    Addresses issues #7 #4
    
    Signed-off-by: antin006 <antin006@morris.umn.edu>

--- client/src/app/todos/todo-list.component.spec.ts
+++ client/src/app/todos/todo-list.component.spec.ts
@@ -1,93 +1,128 @@
-// import { ComponentFixture, TestBed, async } from "@angular/core/testing";
-// import { Todo } from "./todo";
-// import { TodoListComponent } from "./todo-list.component";
-// import { TodoListService } from "./todo-list.service";
-// import { Observable } from "rxjs";
-// import { PipeModule } from "../../pipe.module";
-//
-// describe("Todo list", () => {
-//
-//     let todoList: TodoListComponent;
-//     let fixture: ComponentFixture<TodoListComponent>;
-//
-//     let todoListServiceStub: {
-//         getTodos: () => Observable<Todo[]>
-//     };
-//
-//     beforeEach(() => {
-//         // stub TodoService for test purposes
-//         todoListServiceStub = {
-//             getTodos: () => Observable.of([
-//                 {
-//                     id: "chris_id",
-//                     owner: "Chris",
-//                     status: true,
-//                     category: "UMM",
-//                     body: "this is the email for chris@this.that"
-//                 },
-//                 {
-//                     id: "skye_id",
-//                     owner: "Skye",
-//                     status: false,
-//                     category: "UMM",
-//                     body: "this is the email for skye@this.that"
-//                 },
-//                 {
-//                     id: "brittney_id",
-//                     owner: "Brittney",
-//                     status: true,
-//                     category: "UMM",
-//                     body: "this is the email for brittney@this.that"
-//                 },
-//                 {
-//                     id: "chris2_id",
-//                     owner: "Chris",
-//                     status: false,
-//                     category: "UMM",
-//                     body: "this is the email for chris2@this.that"
-//                 }
-//                 ])
-//         };
-//
-//         TestBed.configureTestingModule({
-//             imports: [PipeModule],
-//             declarations: [ TodoListComponent ],
-//             // providers:    [ TodoListService ]  // NO! Don't provide the real service!
-//             // Provide a test-double instead
-//             providers:    [ { provide: TodoListService, useValue: todoListServiceStub } ]
-//         })
-//     });
-//
-//     beforeEach(async(() => {
-//         TestBed.compileComponents().then(() => {
-//             fixture = TestBed.createComponent(TodoListComponent);
-//             todoList = fixture.componentInstance;
-//             fixture.detectChanges();
-//         });
-//     }));
-//
-//     it("contains all unique owners", () => {
-//         expect(todoList.todos.length).toBe(3);
-//     });
-//
-//     it("contains all the todos", () => {
-//         expect(todoList.todos.length).toBe(3);
-//     });
-//
-//     it("contains a todo named 'Chris'", () => {
-//         expect(todoList.todos.some((todo: Todo) => todo.name === "Chris" )).toBe(true);
-//     });
-//
-//     it("contain a todo named 'Jamie'", () => {
-//         expect(todoList.todos.some((todo: Todo) => todo.name === "Jamie" )).toBe(true);
-//     });
-//
-//     it("doesn't contain a todo named 'Santa'", () => {
-//         expect(todoList.todos.some((todo: Todo) => todo.name === "Santa" )).toBe(false);
-//     });
-//
-//     it("has two todos that are 37 years old", () => {
-//         expect(todoList.todos.filter((todo: Todo) => todo.age === 37).length).toBe(2);
-//     });
-//
-// });
+// some work used from previous lab
+
+import { ComponentFixture, TestBed, async } from "@angular/core/testing";
+import {Todo} from "./todo";
+import {TodoListComponent} from "./todo-list.component";
+import {TodoListService} from "./todo-list.service";
+import { PipeModule } from "../../pipe.module";
+import { Observable } from "rxjs";
+// import {FilterBy} from "./filter.pipe";
+
+describe('testing things', () => {
+    let todoList: TodoListComponent;
+    let fixture: ComponentFixture<TodoListComponent>;
+    let todoServiceStub: {
+        getTodos: () => Observable<Todo[]>
+    };
+
+
+    beforeEach(() => {
+        todoServiceStub = {
+            getTodos: () => Observable.of([
+                {
+                    id: "588959855aac378a2f7119ff",
+                    owner: "Roberta",
+                    status: true,
+                    body: "Incididunt elit cillum laborum sunt sit veniam ullamco sit laboris veniam nulla. Labore labore occaecat dolore et fugiat in do nisi eu incididunt dolor officia adipisicing.",
+                    category: "software design"
+                },
+                {
+                    id: "58895985e131bd26d0576031",
+                    owner: "Fry",
+                    status: false,
+                    body: "Ea id cupidatat magna sint aliquip ut voluptate. Esse occaecat amet id aliquip commodo.",
+                    category: "video games"
+                },
+                {
+                    id: "588959851dcf63c371007691",
+                    owner: "Dawn",
+                    status: true,
+                    body: "Do cillum ipsum esse duis. Labore do ea nisi nisi ut occaecat sint consequat.",
+                    category: "video games"
+                },
+                {
+                    id: "58895985079310e4deeb1444",
+                    owner: "Fry",
+                    status: true,
+                    body: "Adipisicing quis eu dolore mollit labore id nostrud. Mollit eu officia consectetur ea labore commodo ea.",
+                    category: "groceries"
+                },
+                {
+                    id: "58895985ee196f2401e8c52a",
+                    owner: "Roberta",
+                    status: false,
+                    body: "In sunt adipisicing tempor non aliquip ad reprehenderit aute do aliquip deserunt nostrud aute aliquip. Ipsum irure anim excepteur proident irure cillum Lorem occaecat in non non.",
+                    category: "software design"
+                },
+                {
+                    id: "58895985313a7b3a51ca2b40",
+                    owner: "Fry",
+                    status: false,
+                    body: "Anim anim anim non ea consequat amet occaecat nisi est sunt. Eiusmod aliquip nulla duis elit nostrud aute nostrud ex ut proident non.",
+                    category: "groceries"
+                },
+                {
+                    id: "58895985bc042a142189b3ff",
+                    owner: "Barry",
+                    status: true,
+                    body: "Ea fugiat eu exercitation laboris incididunt nulla ullamco qui ad nisi quis pariatur. Exercitation aliqua eiusmod ut velit mollit incididunt do aliquip ad.",
+                    category: "groceries"
+                }
+            ])
+        };
+
+        TestBed.configureTestingModule({
+            imports: [PipeModule],
+            declarations: [TodoListComponent ],
+            providers: [{provide: TodoListService, useValue: todoServiceStub}]
+        })
+    });
+
+    beforeEach(async(() => {
+        TestBed.compileComponents().then(() => {
+            fixture = TestBed.createComponent(TodoListComponent);
+            todoList = fixture.componentInstance;
+            fixture.detectChanges();
+        });
+    }));
+
+
+
+    it('should add things correctly', () =>{
+        expect(2+3).toBe(5);
+    })
+    it('should return the correct number of todos', () =>{
+        expect(todoList.todos.length).toBe(7);
+    })
+    it("Contains an owner named 'Fry'" , () =>{
+        expect(todoList.todos.some((todo:Todo) => todo.owner === "Fry" )).toBe(true);
+    })
+    it("Does not contain an owner named 'Santa'" , () =>{
+        expect(todoList.todos.some((todo:Todo) => todo.owner === "Santa" )).toBe(false);
+    })
+    it("has three todos that have the category groceries", () =>{
+        expect(todoList.todos.filter((todo:Todo) => todo.category === "groceries" ).length).toBe(3);
+    })
+    it("has zero todos that have the category math", () =>{
+        expect(todoList.todos.filter((todo:Todo) => todo.category === "math" ).length).toBe(0);
+    })
+    it("has four complete todos", () =>{
+        expect(todoList.todos.filter((todo:Todo) => todo.status === true ).length).toBe(4);
+    })
+    it("has three incomplete todos", () =>{
+        expect(todoList.todos.filter((todo:Todo) => todo.status === false ).length).toBe(3);
+    })
+    it("contains a certain Id", () =>{
+        expect(todoList.todos.some((todo:Todo) => todo.id === "58895985bc042a142189b3ff" )).toBe(true);
+    })
+    it("Does not contain a certain Id", () =>{
+        expect(todoList.todos.some((todo:Todo) => todo.id === "88895985bc042a142189b3ff" )).toBe(false);
+    })
+    it("contains a certain string in the body", () =>{
+        expect(todoList.todos.filter((todo:Todo) => todo.body === "In sunt adipisicing tempor non aliquip ad reprehenderit aute do aliquip deserunt nostrud aute aliquip. Ipsum irure anim excepteur proident irure cillum Lorem occaecat in non non." ).length).toBe(1);
+    })
+    it("Does not contain a certain string in the body", () =>{
+        expect(todoList.todos.filter((todo:Todo) => todo.body === "bananas" ).length).toBe(0);
+    })
+
+})
